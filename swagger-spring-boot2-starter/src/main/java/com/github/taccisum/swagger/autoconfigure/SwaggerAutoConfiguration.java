36661ed7a351eb96fbc044a54027902bcb1fe790
taccisum <514162920@qq.com>
2019-02-12 11:11:04 +0800

    implement spring boot2 swagger starter

---
+++ swagger-spring-boot2-starter/src/main/java/com/github/taccisum/swagger/autoconfigure/SwaggerAutoConfiguration.java
@@ -0,0 +1,33 @@
+package com.github.taccisum.swagger.autoconfigure;
+
+import com.github.taccisum.swagger.configurer.DocketBuilder;
+import com.github.taccisum.swagger.configurer.concrete.DefaultDescriptionBuilder;
+import com.github.taccisum.swagger.configurer.config.SwaggerProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author tac - liaojf@cheegu.com
+ * @since 2019/2/12
+ */
+@EnableSwagger2
+@EnableConfigurationProperties(SwaggerProperties.class)
+public class SwaggerAutoConfiguration {
+    @Autowired
+    private SwaggerProperties properties;
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Docket docket() {
+        DocketBuilder builder = new DocketBuilder(properties);
+        builder.setDescriptionBuilder(new DefaultDescriptionBuilder(environment, properties.getInfo().getDescription()));
+        return builder.build();
+    }
+}
